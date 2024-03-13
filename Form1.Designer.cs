00e02e1afcb27177a1ef7fa2f90083e8b24b5492
Дыня Орехов <danaran319@gmail.com>
2024-03-13 20:33:27 +0300

    the days of my suffering are almost over

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -25,10 +25,19 @@
         private void InitializeComponent() {
             panel1 = new Panel();
             button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            dataGridView1 = new DataGridView();
+            x = new DataGridViewTextBoxColumn();
+            Y = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // panel1
             // 
+            panel1.BackColor = SystemColors.ControlDarkDark;
             panel1.Location = new Point(80, 57);
             panel1.Name = "panel1";
             panel1.Size = new Size(677, 399);
@@ -36,30 +45,101 @@
             // 
             // button1
             // 
-            button1.Location = new Point(814, 140);
+            button1.Location = new Point(779, 155);
             button1.Name = "button1";
-            button1.Size = new Size(94, 29);
+            button1.Size = new Size(150, 29);
             button1.TabIndex = 1;
-            button1.Text = "button1";
+            button1.Text = "Построить график";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(794, 67);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "R";
+            textBox1.Size = new Size(45, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(794, 112);
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "y1";
+            textBox2.Size = new Size(45, 27);
+            textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(862, 112);
+            textBox3.Name = "textBox3";
+            textBox3.PlaceholderText = "y2";
+            textBox3.Size = new Size(45, 27);
+            textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(861, 67);
+            textBox4.Name = "textBox4";
+            textBox4.PlaceholderText = "Шаг";
+            textBox4.Size = new Size(46, 27);
+            textBox4.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { x, Y });
+            dataGridView1.Location = new Point(981, 57);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(300, 399);
+            dataGridView1.TabIndex = 6;
+            // 
+            // x
+            // 
+            x.HeaderText = "X";
+            x.MinimumWidth = 6;
+            x.Name = "x";
+            x.Width = 125;
+            // 
+            // Y
+            // 
+            Y.HeaderText = "Y";
+            Y.MinimumWidth = 6;
+            Y.Name = "Y";
+            Y.Width = 125;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(914, 600);
+            ClientSize = new Size(1445, 600);
+            Controls.Add(dataGridView1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
             Controls.Add(button1);
             Controls.Add(panel1);
             Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Panel panel1;
         private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn x;
+        private DataGridViewTextBoxColumn Y;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,56 +1,84 @@
+using ScottPlot.AxisLimitCalculators;
+using System.Globalization;
+
 namespace WinFormsApp1 {
     public partial class Form1 : Form {
-
+        readonly ScottPlot.WinForms.FormsPlot fp;
         public Form1() {
-
-
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
             InitializeComponent();
+            fp = new() { Dock = DockStyle.Fill };
 
-            
+        }
+        private static int CountDigitsAfterDecimalPoint(double number) {
+            string numberString = number.ToString("0.#############################");
 
+            // Check if there is a decimal point in the string
+            int decimalPointIndex = numberString.IndexOf('.');
+            int digitsAfterDecimal = 0;
 
+            if (decimalPointIndex != -1) {
+                // Count the digits after the decimal point
+                digitsAfterDecimal = numberString.Length - decimalPointIndex - 1;
+            }
+
+            return digitsAfterDecimal;
         }
 
         private void button1_Click(object sender, EventArgs e) {
+
             // Add the FormsPlot
-            var fp = new ScottPlot.WinForms.FormsPlot() { Dock = DockStyle.Fill };
-            Controls.Add(fp);
+            //Controls.Add(fp);
+            //textBox2.Text = textBox1.Text; 
+            panel1.Controls.Remove(fp);
             var Ctg = new Func<double, double>((x) => 1 / Math.Tan(x));
             List<double> X = new();
             List<double> Y = new();
-            int R = 10;
+            double R = double.Parse(textBox1.Text);
+            double step = double.Parse(textBox4.Text);
+            double y1 = double.Parse(textBox2.Text);
+            double y2 = double.Parse(textBox3.Text);
+            //int R = 2;
+
             ScottPlot.Plot myPlot = fp.Plot;
+            myPlot.Clear();
             panel1.Controls.Add(fp);
 
 
-            for (double y = -100; y <= 100; y += 0.1) {
+            for (double y = y1; y <= y2; y += step) {
                 //X.Add(i);
                 //Y.Add(i * i);
-                y = Math.Round(y, 3);
+                //y = Math.Round(y, 3);
 
                 double x = y * Ctg((Math.PI * y) / (2 * R));
                 //x = Math.Round(x, 2);
-
-                if ((y > 0 && X.Last() < x && X.Count > 0) || (y < 0 && X.Count > 0 && X.Last() > x)) {
-                    var scatter = myPlot.Add.Scatter(X.ToArray(), Y.ToArray());
-                    scatter.MarkerSize = 0;
-                    X.Clear();
-                    Y.Clear();
-                    myPlot = fp.Plot;
+                if (X.Count > 0) {
+                    if ((y > 0 && X.Last() < x) || (y < 0 && X.Last() > x)) {
+                        var scatter = myPlot.Add.Scatter(X.ToArray(), Y.ToArray());
+                        scatter.MarkerSize = 0;
+                        X.Clear();
+                        Y.Clear();
+                        myPlot = fp.Plot;
+                        //myPlot.Add.Text(x + " " + y, x, y);
+                    }
                 }
-                if (Math.Abs(Math.Tan((Math.PI * y) / (2 * R))) > 1e-15 && Math.Abs(Math.Tan((Math.PI * y) / (2 * R))) < 1e15) {
+                if (Math.Abs(Math.Tan((Math.PI * y) / (2 * R))) > 1e-10 && Math.Abs(Math.Tan((Math.PI * y) / (2 * R))) < 1e10) {
                     //myPlot.Add.Text(x + " " + $"{Math.Abs(Math.Tan((Math.PI * y) / (2 * R)))} " + y, x, y);
-                    Y.Add(Math.Round(y, 5));
-                    X.Add(Math.Round(x, 5));
-                    //myPlot.Add.Text(x + " " + y, x, y);
+                    Y.Add(y);
+                    X.Add(x);
+                    int digitsAfterDecimal = CountDigitsAfterDecimalPoint(step);
+                    dataGridView1.Rows.Add(x.ToString($"F{digitsAfterDecimal}"), y.ToString($"F{digitsAfterDecimal}"));
                 }
                 //myPlot.Add.Marker(x: y, y: x);
 
             }
-            //myPlot.Add.Scatter(X.ToArray(), Y.ToArray());
+            if (X.Count > 0) {
+                var scatter = myPlot.Add.Scatter(X.ToArray(), Y.ToArray());
+                scatter.MarkerSize = 0;
+            }
 
-            //var scatter = myPlot.Add.Scatter(X.ToArray(), Y.ToArray());
-            //scatter.MarkerSize = 0;
+            //var scatter2 = myPlot.Add.Scatter(X.ToArray(), Y.ToArray());
+            //scatter2.MarkerSize = 0;
         }
     }
 }
